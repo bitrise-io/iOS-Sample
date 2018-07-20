eb4f9b7ae5d077c1c653315a588d5214974eb63f
Akos Birmacher <akos.birmacher@bitrise.io>
2018-07-20 14:39:05 +0200

    Initial Commit

---
+++ iOS Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS Sample
+//
+//  Created by Akos Birmacher on 2018. 07. 20..
+//  Copyright © 2018. Bitfall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
