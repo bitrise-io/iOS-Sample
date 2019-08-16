12633f8ba7e9a238a8a7bd011c469cae4af5b70c
lpusok <7979773+lpusok@users.noreply.github.com>
2019-08-16 09:31:16 +0200

    Tool-248 firebase (#7)
    * Adding Firebase Crashlytics sample
    
    * Update Fabric Crashalytics sample
    
    * Enable DWARF with dSYM, disable bitcode, migrate to Swift 5
    
    * Fix Crashalytics integration, adding Firebase pod
    
    * Add shared scheme
    
    * Removing firebase crashlytics sample

--- iOS Sample/ViewController.swift
+++ iOS Sample/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import Crashlytics
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let button = UIButton(type: .roundedRect)
+        button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
+        button.setTitle("Crash", for: [])
+        button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
+        view.addSubview(button)
+    }
+    
+    @IBAction func crashButtonTapped(_ sender: AnyObject) {
+        Crashlytics.sharedInstance().crash()
     }
 
     override func didReceiveMemoryWarning() {
